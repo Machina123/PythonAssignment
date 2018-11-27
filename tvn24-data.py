173229b71d97f3c1914a4a348979526a5e8890c0
BartekGilus <BartekGilus@users.noreply.github.com>
2018-11-27 16:44:01 +0100

    Update tvn24-data.py
    pobieranie wiadomosci z tv24

--- tvn24-data.py
+++ tvn24-data.py
@@ -1,1 +1,19 @@
-from bs4 import BeautifulSoup
+import bs4 as bs
+import urllib.request
+
+url = 'https://www.tvn24.pl/najnowsze.xml'
+sauce = urllib.request.urlopen(url).read()
+soup = bs.BeautifulSoup(sauce, 'xml')
+tab = []
+
+for url in soup.find_all('item'):
+    xd = {
+        "title": url.title.text,
+        "link": url.link.text,
+        "desc": url.description.text,
+        "pubDate": url.pubDate.text
+    }
+
+    tab.append(xd)
+
+print(tab[0]["title"])
