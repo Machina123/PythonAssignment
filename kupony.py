261d7a58d949f438941c53b74ced24aed0e9f850
Paarzivall <44908654+Paarzivall@users.noreply.github.com>
2018-11-27 18:19:39 +0100

    kupony do McDonalds

---
+++ kupony.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def mac():
+
+    with urllib.request.urlopen('https://mcdonalds.pl/oferty/?utm_source=mcdonalds.pl&utm_medium=slider&utm_campaign=kupony') as response:
+        webpage = response.read()
+        soup = BeautifulSoup(webpage, 'html.parser')
+        kuponyMac = {}
+        nazwaKuponu = []
+        cenaKuponu = []
+        fotoKuponu = []
+        for anchor in soup.find_all('div', {'class': 'coupon'}):
+            for x in anchor.find_all('a', {'class': 'download-coupon'}):
+                for y in x.find_all('h2'):
+                    nazwaKuponu.append(y.get_text())
+
+                for ceny in anchor.find_all('div', {'class': 'price'}):
+                    for u in ceny.find_all('span', {'class': 'pln'}):
+                        cenaKuponuTMP = u.get_text()
+                    for v in ceny.find_all('span', {'class': 'gr'}):
+                        nowaCena = cenaKuponuTMP+"."+v.get_text()
+                        cenaKuponu.append(nowaCena)
+                for img in anchor.find_all('img'):
+                    fotoKuponu.append(img)
+
+        for i in fotoKuponu:
+            print(i)
+
+
+    cenaKuponu.reverse()
+    x = len(cenaKuponu)
+    for i in nazwaKuponu:
+        for j in cenaKuponu:
+            kuponyMac[i] = j
+        cenaKuponu.remove(j)
+    for i in nazwaKuponu:
+         print(i, kuponyMac[i])
+
+
+mac()
