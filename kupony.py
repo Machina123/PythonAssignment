36ae268d8ad34f530a74341c11ac1255fef3a11e
Parzivall <mateusz.bugaj@interia.pl>
2018-11-30 09:59:01 +0100

    propably all

--- kupony.py
+++ kupony.py
@@ -52,28 +52,63 @@ def mac():
          # print(i, kuponyMac[i])
 
 
-def kfc():
-    with urllib.request.urlopen('https://kfc.pl/main/home/coupons') as response:
+
+def subway():
+    with urllib.request.urlopen('https://www.qpony.pl/subway') as response:
         webpage = response.read()
         soup = BeautifulSoup(webpage, 'html.parser')
-        kuponyKFC = []
-        kuponyKFCEl = {}
-        nazwaKuponu = []
+        nazwaCenaSubway = []
         fotoKuponu = []
+        kuponySubwayEl = {}
+        kuponySubway = []
+        for x in soup.find_all('div', {'class': 'layout-with-sidebar__content'}):
+            for y in x.find_all('div', {'class': 'section__qpons'}):
+                for z in y.find_all('div', {'class': 'qpon-horizontal'}):
+                    for a in z.find_all('div', {'class': 'qpon-horizontal__name'}):
+                        nazwaCenaSubway.append(a.get_text())
+                    for b in z.find_all('div', {'class': 'qpon-horizontal__thumb'}):
+                        fotoKuponu.append(b.get('data-original'))
 
-        for anchor in soup.find_all('div', {'class': 'inner-container-bricks'}):
-            for x in anchor.find_all('div', {'class': 'coupon-container'}):
-                for y in x.find_all('div', {'class': 'coupon-content-text'}):
-                    print(y.get_text())
 
-                # for y in x.find_all('img'):
-                    # fotoKuponu.append(y.get('src'))
+        for i in nazwaCenaSubway:
+            for j in fotoKuponu:
+                kuponySubwayEl = {
+                    "name": i,
+                    "picture": j
+                }
+            kuponySubway.append(kuponySubwayEl)
 
-        for i in fotoKuponu:
+        with open('kuponySubway.json', 'w') as outfile:
+            for i in kuponySubway:
+                # print(i)
+                json.dump(i, outfile)
+
+def pizza():
+    with urllib.request.urlopen('https://www.dagrasso.pl/promocje') as response:
+        webpage = response.read()
+        soup = BeautifulSoup(webpage, 'html.parser')
+        fotoKuponu = []
+        nazwaKuponu = []
+        for u in soup.find_all('div', {'class': 'content-promo'}):
+            for w in u.find_all('div', {'class': 'container'}):
+                for x in w.find_all('div', {'class': 'row'}):
+                    for y in x.find_all('div'):
+                        for z in y.find_all('img'):
+                            fotoKuponu.append(z.get('src'))
+                        for nazwa in y.find_all('h1'):
+                            nazwaKuponu.append(nazwa.get_text())
+
+        for i in nazwaKuponu:
             print(i)
 
-# print("\t\tMAC\n")
-mac()
 
-print("\n\t\tKFC\n")
-# kfc()
+# print("\t\tMAC\n")
+# mac()
+
+# print("\n\t\tSUBWAY\n")
+# subway()
+
+print("\n\t\tDominium\n")
+pizza()
+
+
