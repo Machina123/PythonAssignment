cb0340d7e945605b86050ce13094a5fdc75fb360
BartekGilus <BartekGilus@users.noreply.github.com>
2018-12-01 13:35:59 +0100

    XD

--- tvn24-data.py
+++ tvn24-data.py
@@ -1,9 +1,14 @@
 import bs4 as bs
 import urllib.request
 
-url = 'https://www.tvn24.pl/najnowsze.xml'
-sauce = urllib.request.urlopen(url).read()
-soup = bs.BeautifulSoup(sauce, 'xml')
+urlTvn = 'https://www.tvn24.pl/najnowsze.xml'
+
+def get_soup(url):
+    sauce = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(sauce, 'xml')
+    return soup
+
+soup = get_soup(urlTvn)
 tab = []
 
 for url in soup.find_all('item'):
