9e25369121e2e4545f4e1732c2601e50fcdbe211
BartekGilus <BartekGilus@users.noreply.github.com>
2018-12-02 14:37:51 +0100

    Plik urls i chyba działające wyciąganie wiadomości

--- PythonAssignment/urls.py
+++ PythonAssignment/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
-    path('', views.index),
     path('info/', include('views.urls')),
     path('admin/', admin.site.urls),
 ]
