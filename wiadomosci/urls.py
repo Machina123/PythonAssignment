b0a5a0e68507115ca8973fc4d7d1c74d498db805
BartekGilus <BartekGilus@users.noreply.github.com>
2018-12-02 14:09:34 +0100

    Plik urls i chyba działające wyciąganie wiadomości

--- wiadomosci/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import wiadomosci
-
-urlpatterns = [
-    path('tv', wiadomosci.tv, name="tvn"),
-    path('rmf', wiadomosci.rmf, name="rmf"),
-    path('interia', wiadomosci.interia, name="interia")
-]
