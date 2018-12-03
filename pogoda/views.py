615c8c0cb0f71f6233af8892d0f5316ce7dd6ee9
BartekGilus <BartekGilus@users.noreply.github.com>
2018-12-03 18:45:26 +0100

    Pogoda(Chyba działa)

--- pogoda/views.py
+++ pogoda/views.py
@@ -6,7 +6,7 @@ import json
 api_key = "c26cd8d618c32f318fef96a3b9756d1c"
 lat = -31.967819
 lng = 115.8771
-url = "https://api.darksky.net/forecast/"+api_key+"/"+str(lat)+","+str(lng)+""
+url = "https://api.darksky.net/forecast/"+api_key+"/"+str(lat)+","+str(lng)+"?lang=pl"
 
 def pogoda(request):
     json_data = requests.get(url).json()
