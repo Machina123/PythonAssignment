d4b2d84a5b021ac0611750ea67d37abb5b4a2ba4
BartekGilus <BartekGilus@users.noreply.github.com>
2018-12-03 18:41:20 +0100

    Pogoda(Chyba działa)

--- pogoda/views.py
+++ pogoda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse, HttpResponseRedirect
+import json
 
 api_key = "c26cd8d618c32f318fef96a3b9756d1c"
 lat = -31.967819
@@ -9,4 +10,4 @@ url = "https://api.darksky.net/forecast/"+api_key+"/"+str(lat)+","+str(lng)+""
 
 def pogoda(request):
     json_data = requests.get(url).json()
-    return HttpResponse(json_data['currently'])
+    return HttpResponse(json.dumps(json_data['currently']))
