5b6ef5ca563f8d615ed2417b66b1f8870fe2cc2b
Patryk Ciepiela <machina123@boun.cr>
2018-12-04 20:38:27 +0100

    Front-end MPK data download almost working

--- static/script/mpk.js
+++ static/script/mpk.js
@@ -4,7 +4,33 @@ function getMpkStops() {
         type: 'GET',
         dataType: 'json',
         success: function(data, status, xhr) {
-            console.log(data)          
+            var stopPicker = document.getElementById("przystanek");
+            stopPicker.innerHTML = "";
+            for(var key in data) {
+                var stopObject = document.createElement("option");
+                stopObject.innerText = key;
+                stopObject.setAttribute("value", data[key]);
+                stopPicker.appendChild(stopObject);
+            }        
+        }
+    })
+}
+
+function getMpkSchedule() {
+    var stopId = $("#przystanek").val()
+    $.ajax({
+        url: "http://localhost:8080/mpk/departures?stop=" + stopId,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data, status, xhr) {
+            var departuresPlaceholder = document.getElementById("mpk_rozklad");
+            departuresPlaceholder.innerHTML = "";
+            for(var i = 0; i < data.actual.length; i++) {
+                var departData = data.actual[i];
+                var departure = document.createElement("p")
+                departure.innerHTML = departData.patternText + " &rarr; " + departData.direction + ": " + departData.mixedTime
+                departuresPlaceholder.appendChild(departure);
+            }       
         }
     })
 }
--- szukajka.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-def szukajka(klucz):
-    webbrowser.open("http://www.google.pl/search?q="+klucz)
-    open("link.html", "w").write('<a href="http://www.google.pl/"+klucz+"> Link </a>')
-
-print("Podaj szukaną frazę:\t")
-klucz = input()
-szukajka(klucz)
