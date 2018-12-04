b04e30b7ece070ebdfdbc95a24a702cd477e9965
Paarzivall <44908654+Paarzivall@users.noreply.github.com>
2018-12-04 22:28:13 +0100

    kupony

--- coupons/kupony-backup/kupony-Mac.py
+++ coupons/kupony-backup/kupony-Mac.py
@@ -17,47 +17,42 @@ def mac():
                 for y in x.find_all('h2'):
                     nazwaKuponu.append(y.get_text())
 
-                for ceny in anchor.find_all('div', {'class': 'price'}):
-                    for u in ceny.find_all('span', {'class': 'pln'}):
-                        cenaKuponuTMP = u.get_text()
-                    for v in ceny.find_all('span', {'class': 'gr'}):
-                        nowaCena = cenaKuponuTMP+"."+v.get_text()
-                        cenaKuponu.append(nowaCena)
+                if len(anchor.find_all('div', {'class': 'price'})) < 1:
+                    cenaKuponu.append(' ')
+                else:
+                    for ceny in anchor.find_all('div', {'class': 'price'}):
+                        for u in ceny.find_all('span', {'class': 'pln'}):
+                            cenaKuponuTMP = u.get_text()
+                        for v in ceny.find_all('span', {'class': 'gr'}):
+                            nowaCena = cenaKuponuTMP + "." + v.get_text()
+                            cenaKuponu.append(nowaCena)
                 for img in anchor.find_all('img'):
-                    # fotoKuponu.append(img)
-                    fotoKuponu.append(img.get('src'))# pobranie zawartości alt'a
+                    fotoKuponu.append(img.get('src'))  # pobranie zawartości alt'a
 
-    cenaKuponu.reverse()
-    fotoKuponu.reverse()
-    for i in nazwaKuponu:
-        for j in cenaKuponu:
-            for k in fotoKuponu:
-                kuponyMacEl = {
-                    "name": i,
-                    "value": j,
-                    "picture": k
-                }
-        # print(kuponyMacEl)
-            fotoKuponu.remove(k)
-            cenaKuponu.remove(j)
-            kuponyMac.append(kuponyMacEl)
+    # cenaKuponu.reverse()
+    # fotoKuponu.reverse()
+    # for i in nazwaKuponu:
+    #     for j in cenaKuponu:
+    #         for k in fotoKuponu:
+    #             kuponyMacEl = {
+    #                 "name": i,
+    #                 "value": j,
+    #                 "picture": k
+    #             }
+    #     # print(kuponyMacEl)
+    #         fotoKuponu.remove(k)
+    #         cenaKuponu.remove(j)
 
-
+    for i in range(len(nazwaKuponu)):
+        kuponyMacEl = {
+            "name": nazwaKuponu[i],
+            "price": cenaKuponu[i],
+            "picture": fotoKuponu[i]
+        }
+        kuponyMac.append(kuponyMacEl)
 
     for i in kuponyMac:
         print(i)
-   # with open('kuponyMcDonald.json', 'w') as outfile:
-        # for i in kuponyMac:
-            # print(i)
-           # json.dump(i, outfile)
-
-    # cenaKuponu.reverse()
-    # x = len(cenaKuponu)
-    # for i in nazwaKuponu:
-        # for j in cenaKuponu:
-            # kuponyMac[i] = j
-        # cenaKuponu.remove(j)
-    # for i in nazwaKuponu:
-         # print(i, kuponyMac[i])
+    #return HttpResponse(json.dumps(kuponyMac))
 
 mac()
--- static/script/kupony.js
+++ static/script/kupony.js
@@ -4,7 +4,30 @@ function getKuponyMac() {
         type: 'GET',
         dataType: 'json',
         success: function(data, status, xhr) {
-            console.log(data)          
+            console.log(data)
+           /* var imgInWeb = document.getElementById("imgMc");
+            var imgInWebInd = document.getElementById("olMac");
+            for(var j = 0; j < data.lenght; j++){
+                if(j == 0)
+                    imgInWebInd.innerHTML = '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'" class="active"></li>';
+                else 
+                    imgInWebInd.innerHTML = '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'"></li>';
+            }
+
+            for(var i = 0; i < data.lenght; i++){
+                var div = document.createElement("div");
+                if(i == 0)
+                    div.className = "carousel-item active";
+                else
+                    div.className = "carousel-item";
+                
+                div.innerHTML = '<img src="' +data[i].picture+'" alt="Picture Mc">';
+                imgInWeb.appendChild(div);
+            }
+            $("#carouselExampleIndicatorsMac").carousel();*/
+            //for(var i = 0; i < data.name.length; i++){
+             //   console.log(data.name[i])
+           // }
         }
     })
 }
@@ -20,7 +43,7 @@ function getKuponySubway() {
     })  
 }
 
-function getKuponyMac() {
+function getKuponyDagrasso() {
     $.ajax({
         url: "http://localhost:8080/coupons/dagrasso",
         type: 'GET',
