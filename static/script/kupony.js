2591da014eebdca021ddb0b6133c6fe00fce6fad
Parzivall <mateusz.bugaj@interia.pl>
2018-12-05 10:26:48 +0100

    update

--- static/script/kupony.js
+++ static/script/kupony.js
@@ -4,30 +4,32 @@ function getKuponyMac() {
         type: 'GET',
         dataType: 'json',
         success: function(data, status, xhr) {
-            console.log(data)
-           /* var imgInWeb = document.getElementById("imgMc");
+            //console.log(data)
+			var imgInWeb = document.getElementById("imgMc");
             var imgInWebInd = document.getElementById("olMac");
-            for(var j = 0; j < data.lenght; j++){
+			var classTxt = '<div class="carousel-caption d-none d-md-block text-right kuponImg">';
+			imgInWebInd.innerHTML='';
+            for(var j = 0; j < data.length; j++){
                 if(j == 0)
-                    imgInWebInd.innerHTML = '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'" class="active"></li>';
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'" class="active"></li>';
                 else 
-                    imgInWebInd.innerHTML = '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'"></li>';
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'"></li>';
             }
 
-            for(var i = 0; i < data.lenght; i++){
+            for(var i = 0; i < data.length; i++){
                 var div = document.createElement("div");
                 if(i == 0)
                     div.className = "carousel-item active";
                 else
                     div.className = "carousel-item";
                 
-                div.innerHTML = '<img src="' +data[i].picture+'" alt="Picture Mc">';
-                imgInWeb.appendChild(div);
-            }
-            $("#carouselExampleIndicatorsMac").carousel();*/
-            //for(var i = 0; i < data.name.length; i++){
-             //   console.log(data.name[i])
-           // }
+                div.innerHTML = 
+				'<img class="d-block w-75 image mx-auto" src="' +data[i].picture+'" alt="Picture Mc">'+
+                classTxt+'<h5>'+data[i].name+'</h5><p>'+data[i].price+'</p></div>';	
+				imgInWeb.appendChild(div);
+            
+			}
+            $("#carouselExampleIndicatorsMac").carousel();
         }
     })
 }
@@ -38,7 +40,32 @@ function getKuponySubway() {
         type: 'GET',
         dataType: 'json',
         success: function(data, status, xhr) {
-            console.log(data)          
+            //console.log(data)
+			var imgInWeb = document.getElementById("imgSub");
+            var imgInWebInd = document.getElementById("olSub");
+			var classTxt = '<div class="carousel-caption d-none d-md-block text-right kuponImg">';
+			imgInWebInd.innerHTML='';
+			            for(var j = 0; j < data.length; j++){
+                if(j == 0)
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'" class="active"></li>';
+                else 
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'"></li>';
+            }
+
+            for(var i = 0; i < data.length; i++){
+                var div = document.createElement("div");
+                if(i == 0)
+                    div.className = "carousel-item active";
+                else
+                    div.className = "carousel-item";
+                
+                div.innerHTML = 
+				'<img class="d-block w-75 image mx-auto" src="' +data[i].picture+'" alt="Picture Mc">'+
+                classTxt+'<h5>'+data[i].name+'</h5></div>';	
+				imgInWeb.appendChild(div);
+            
+			}
+            $("#carouselExampleIndicatorsMac").carousel();
         }
     })  
 }
@@ -49,7 +76,32 @@ function getKuponyDagrasso() {
         type: 'GET',
         dataType: 'json',
         success: function(data, status, xhr) {
-            console.log(data)          
+            //console.log(data)  
+			var imgInWeb = document.getElementById("imgDag");
+            var imgInWebInd = document.getElementById("olDag");
+			var classTxt = '<div class="carousel-caption d-none d-md-block text-right kuponImg">';
+			imgInWebInd.innerHTML='';
+			            for(var j = 0; j < data.length; j++){
+                if(j == 0)
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'" class="active"></li>';
+                else 
+                    imgInWebInd.innerHTML += '<li data-target="#carouselExampleIndicatorsMac" data-slide-to="'+j+'"></li>';
+            }
+
+            for(var i = 0; i < data.length; i++){
+                var div = document.createElement("div");
+                if(i == 0)
+                    div.className = "carousel-item active";
+                else
+                    div.className = "carousel-item";
+                
+                div.innerHTML = 
+				'<img class="d-block w-75 image mx-auto" src="' +data[i].picture+'" alt="Picture Mc">'+
+                classTxt+'<h5>'+data[i].name+'</h5></div>';	
+				imgInWeb.appendChild(div);
+            
+			}
+            $("#carouselExampleIndicatorsMac").carousel();
         }
     })
 }
